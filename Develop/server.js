6856a6d613c8d5f599c8a52380c2b650594a064e
Aliriza Bagdat <ambagdat@gmail.com>
2023-12-14 18:16:02 +0000

    middleware created

--- Develop/server.js
+++ Develop/server.js
@@ -1,3 +1,11 @@
 const express = require('express')
 const fs = require('fs')
 const path = require('path')
+
+const app = express();
+const PORT = 3000;
+
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.static('public'));
